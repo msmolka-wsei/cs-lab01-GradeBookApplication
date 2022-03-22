504c975d13545c8e80c50ee34524f69e7fefbadd
msmolka-wsei <michal.smolka@microsoft.wsei.edu.pl>
2022-03-22 19:47:46 +0100

    CalculateStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,7 +12,25 @@ namespace GradeBook.GradeBooks
             Type = Enums.GradeBookType.Ranked;
         }
 
-        /*
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int numberOfStudents = Students.Count;
+
+            double N = Convert.ToDouble(numberOfStudents / 5);
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+            else if (averageGrade >= N)
+                return 'A';
+            else if (averageGrade >= 2*N)
+                return 'B';
+            else if (averageGrade >= 3*N)
+                return 'C';
+            else if (averageGrade >= 4*N)
+                return 'D';
+            else
+                return 'F';
+        }
+        
         public override void CalculateStatistics()
         {
             if (Students.Count < 5)
@@ -22,9 +40,22 @@ namespace GradeBook.GradeBooks
 
             else
             {
-                base.CalculateStudentStatistics(Name);
+                base.CalculateStatistics();
             }
         }
-        */
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
+
     }
 }
